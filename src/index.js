9d6f39f1542de4257e83062c9c47ffb6a1a4cc26
0codezone <mohittailor997@gmail.com>
2024-02-24 17:32:04 +0530

    utils code discussion

--- src/index.js
+++ src/index.js
@@ -6,8 +6,20 @@ import connectDB from "./db/index.js";
 dotenv.config({ path: "./env" });
 
 //database connection
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", (err) => {
+      console.log("ERROR: ", err);
+      throw err;
+    });
 
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("ERROR: ", err);
+  });
 // ------------------ METHOD 1------------------->
 /*
 import mongoose from "mongoose";
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,24 @@
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((error) => {
+      next(error);
+    });
+  };
+};
+
+export default asyncHandler;
+
+// ------------mthod 2 using trycathch block----------------
+// const asyncHandler = (fn) => async (req, res, next) => {
+//   try {
+//     await fn(req, res, next);
+//   } catch (error) {
+//     console.log(" ERROR: ", error);
+//     res.status(error.code || 500).json({
+//       success: false,
+//       message: error.message || "Internal server error",
+//     });
+//   }
+// };
+
+// export default asyncHandler;
