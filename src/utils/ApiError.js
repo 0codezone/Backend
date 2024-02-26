b2d65d9a9f6d7574aa7291edd11b25f0b5b2ead7
0codezone <mohittailor997@gmail.com>
2024-02-26 19:47:34 +0530

    setup image upload using cloudinary and filesystem and multer middleware

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -3,7 +3,7 @@ class ApiError extends Error {
     statusCode,
     message = "Internal server error",
     errors = [],
-    statck = ""
+    stack = ""
   ) {
     super(message);
     this.statusCode = statusCode;
@@ -13,8 +13,8 @@ class ApiError extends Error {
     this.errors = errors;
     this.stack = stack;
 
-    if (statck) {
-      this.statck = statck;
+    if (stack) {
+      this.stack = stack;
     } else {
       Error.captureStackTrace(this, this.constructor);
     }
